8519c61c2c386d37c4f0a29896cab71a5877a46d
terror2012 <megaimpact2012@yahoo.com>
2017-02-07 06:12:42 +0200

    Sys_Off added new general entry in case general is empty + new check on sys_off for general_settings

--- laravel/app/Http/Middleware/sys_off.php
+++ laravel/app/Http/Middleware/sys_off.php
@@ -17,17 +17,31 @@ class sys_off
      */
     public function handle($request, Closure $next)
     {
-        if(Auth::check())
-        {
-            $gen = general_settings::find('1')->first();
+            $gen = general_settings::find('1');
             if($gen !== null)
             {
+                $gen->first();
                 if($gen->offline_mode == '1')
                 {
                     return redirect()->route('offline');
                 }
             }
-        }
+            else
+            {
+                $g = new general_settings();
+                $g->id = '1';
+                $g->phone = '0';
+                $g->email = 'test@test.com';
+                $g->logo = 'img/logo.png';
+                $g->rate_hour = '40';
+                $g->days = 'Mon-Sat';
+                $g->hours = '10AM-3PM';
+                $g->initial_payment = '15';
+                $g->email_on_upgrade = '1';
+                $g->offline_mode = '0';
+                $g->save();
+                return redirect()->route('home');
+            }
         return $next($request);
 
     }
